efb16db101fa6d285934b67ed7ec9a22a3ec1ec9
prajwaldilipbhagat@gmail.com <prajwaldilipbhagat@gmail.com>
2025-06-28 13:59:33 +0530

    js files

---
+++ JavaScript/Day-2/index.js
@@ -0,0 +1,18 @@
+// Create your product object
+// const product = {
+// 	id: 1,
+// 	name: 'apple',
+// 	price: 999.99,
+// 	availability: true
+// }
+// export your product object
+;
+export const product = {
+	id: 1,
+	name: 'apple',
+	price: 999.99,
+	availability: true
+}
+// console.log the object to view the output
+console.log(product)
+// TIP: Use the run button to run your JS Code
